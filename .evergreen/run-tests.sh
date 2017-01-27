6a2a324e864530c08e927f96518fa51f172bd499
Emily <emily@mongodb.com>
2017-01-27 17:26:31 +0100

    Take out line to install ruby from test script

--- .evergreen/run-tests.sh
+++ .evergreen/run-tests.sh
@@ -4,15 +4,12 @@ set -o xtrace   # Write all commands first to stderr
 set -o errexit  # Exit the script with error if any of the commands fail
 
 # Supported/used environment variables:
-#       RUBY                    Define the Ruby version to test with, using its RVM identifier.
+#       RVM_RUBY                Define the Ruby version to test with, using its RVM identifier.
 #                               For example: "ruby-2.3" or "jruby-9.1"
 
 RVM_RUBY=${RVM_RUBY:-}
 
 source ~/.rvm/scripts/rvm
-
-# echo "Installing $RUBY"
-rvm install $RVM_RUBY
 
 echo "Installing bundler"
 rvm use $RVM_RUBY
